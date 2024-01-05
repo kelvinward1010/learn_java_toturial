c65092f02a4f4f7b6d3dcb8ccff2c5aa0b9e60c3
Kelvin Ward <kelvinward1010@gmail.com>
2024-01-05 23:18:46 +0700

    first commit

---
+++ helloworld.java
@@ -0,0 +1,18 @@
+class Main {
+    public static void main(String[] args) {
+
+        System.out.println();
+        System.out.println("Hello World!\nMy name is Kelvin");
+        System.out.print("It's so good!");
+        System.out.println("\tIt's so bad!");
+        System.out.println("\"I love the world!\"");
+        System.out.println();
+        System.out.println();
+
+        //This is a comment
+
+        /*
+         This is mutiline comment
+        */
+    }
+}
