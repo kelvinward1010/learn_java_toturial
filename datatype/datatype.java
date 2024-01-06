70e407744422b985fc6a730742e8de161e1a932c
Kelvin Ward <kelvinward1010@gmail.com>
2024-01-06 13:08:37 +0700

    update

---
+++ datatype/datatype.java
@@ -0,0 +1,24 @@
+package datatype;
+
+public class datatype {
+    public static void main(String[] args) {
+        exampleType.main(args);
+    }
+}
+
+
+class exampleType {
+    static void main(String[] args) {
+        int number = 5;               // Integer (whole number)
+        float floatValue = 5.99f;    // Floating point number
+        char charValue = 'D';         // Character
+        boolean boolValue = true;       // Boolean
+        String textValue = "Hello";     // String
+
+        System.out.println(number);
+        System.out.println(floatValue);
+        System.out.println(charValue);
+        System.out.println(boolValue);
+        System.out.println(textValue);
+    }
+}
--- variables/variable.java
+++ variables/variable.java
@@ -1,5 +1,46 @@
 package variables;
 
 public class variable {
-    
+    public static void main(String[] args) {
+        variableservice.show_2();
+    }
+}
+
+class variableservice {
+    static void show_1() {
+        String name = "Kelvin";
+        System.err.println(name);
+
+        int number;
+        number = 1010;
+        System.out.println(number);
+
+        float floatValue = 1.99f;
+        System.out.println(floatValue);
+
+        char charValue = 'D';
+        System.out.println(charValue);
+
+        boolean booleanValue = false;
+        System.out.println(booleanValue);
+    }
+
+    static void show_2() {
+        String name = "Kelvin";
+        System.out.println("My name is " + name);
+
+        String firstname = "Kelvin";
+        String lastname = "Ward";
+        String fullname = firstname + ' ' + lastname;
+        System.out.println(fullname);
+
+        int x = 5;
+        int y = 6;
+        System.out.println(x + y);
+
+        int x2 = 5;
+        int y2 = 6;
+        int z2 = 50;
+        System.out.println(x2 + y2 + z2);
+    }
 }
